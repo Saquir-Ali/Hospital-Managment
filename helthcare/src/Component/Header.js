bc12245c0209487a3e0c3f20722bb7c3d0473410
saquir ali <shakirali9581@gmail.com>
2024-09-05 10:59:30 +0530

    changes dashloard component

--- helthcare/src/Component/Header.js
+++ helthcare/src/Component/Header.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 const Header = () => {
+  const params = useParams();
+  console.log(params);
+  return (
+    <header className="flex items-center justify-between mb-4">
+      <h2 className="text-3xl font-bold">Dashboard</h2>
+      <div className="flex items-center space-x-4">
+        <span className="font-semibold">Jonitha Cathrine (Admin)</span>
+        <div className="relative">
+          <span className="w-3.5 h-3.5 bg-red-500 rounded-full absolute top-0 right-0"></span>
+          <img
+            src="/path/to/avatar.jpg"
+            alt="Avatar"
+            className="w-10 h-10 rounded-full"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-    const params =useParams();
-    console.log(params)
-    return (
-
-        <header className="flex items-center justify-between mb-6">
-            <h2 className="text-3xl font-bold">Dashboard</h2>
-            <div className="flex items-center space-x-4">
-                <span className="font-semibold">Jonitha Cathrine (Admin)</span>
-                <div className="relative">
-                    <span className="w-3.5 h-3.5 bg-red-500 rounded-full absolute top-0 right-0"></span>
-                    <img src="/path/to/avatar.jpg" alt="Avatar" className="w-10 h-10 rounded-full" />
-                </div>
-            </div>
-        </header>
-
-    )
-}
-
-export default Header
+export default Header;
